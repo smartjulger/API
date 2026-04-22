d3daf62128e3846cb142581cc11903bbe0dcc8e5
julius <Julger04@gmail.com>
2026-04-22 10:10:47 +0200

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    output:"server"
+    adapter: node({ mode:'standalone'})
 });
+
